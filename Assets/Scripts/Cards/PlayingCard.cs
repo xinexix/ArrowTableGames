69c8b7378ec79cb2694a632124faa92e6130f5e3
xinexix <xinexix@hotmail.com>
2021-02-21 12:33:11 -0800

    feat(card): update playing card name to match sprite names
    (cherry picked from commit 82a583a33995434028b33dd05b2ceb99893a845b)

--- Assets/Scripts/Cards/PlayingCard.cs
+++ Assets/Scripts/Cards/PlayingCard.cs
@@ -23,7 +23,7 @@ public class PlayingCard
 
         string suffix = nameof(_suit);
 
-        return $"{prefix} of {suffix}s";
+        return $"{prefix}_of_{suffix}s";
     }
 
     public CardRank rank => _rank;

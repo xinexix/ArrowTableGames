e0ccfe9046ac5707e57c96cfea4124e054be9428
xinexix <xinexix@hotmail.com>
2021-02-26 22:27:10 -0800

    fix: protect against exception when script not hooked up

--- Assets/Scripts/Lobby/ShowOnActiveGame.cs
+++ Assets/Scripts/Lobby/ShowOnActiveGame.cs
@@ -10,6 +10,12 @@ public class ShowOnActiveGame : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        if (lobbyController == null)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
         lobbyController.onGameStarted += handleGameStarted;
         lobbyController.onGameExited += handleGameExisted;
     }
